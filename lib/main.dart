48e4ca8f4a87474dbb1f06b1dc213caa904feb8b
MennaCC <menna.elsisy@gmail.com>
2023-02-16 17:21:02 +0200

    Adding AppIcon using new image asset method

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,25 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: Center(
-      child: Text("Hello"),
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "I Am Rich gdn",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+              fontWeight: FontWeight.w500,
+              fontFamily: 'Roboto',
+              color: Colors.white),
+        ),
+        backgroundColor: Colors.purple[700],
+      ),
+      backgroundColor: Colors.black87,
+      body: const Center(
+        child: Image(
+          image: AssetImage('images/diamond-res.png'),
+        ),
+      ),
     ),
   ));
 }
